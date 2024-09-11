45b3e184feefdaa50836a540b61bcbd3c4d0c650
Sivapragasam <qc.siva@gmail.com>
2024-09-11 18:10:48 +0530

    Logo Not yet Finished

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,28 +3,45 @@ import { Link } from "react-router-dom"
 function Login() {
   return (
     <>
-      <div className="basebox justify-content-center d-flex flex-row" style={{ backgroundColor: "skyblue", height: "100vh", width: "auto" }}>
-        <div className="row col-lg-12">
-          <div className="container-fluid " style={{ backgroundColor: "dodgerblue", height: "100vh", width: "100vh" }}>
+      <div className="basebox container-fluid d-flex flex-row" style={{ backgroundColor: "skyblue", height: "100vh", width: "auto" }}>
+        <div className="row col-lg-12 d-flex ">
+          <div className="container-fluid col-lg-6 " style={{ backgroundColor: "dodgerblue", height: "100vh", width: "100vh" }}>
             <div className="row">
-              <div className="col-lg-12">
-                <div className="container fst-italic" style={{ marginLeft: "200px", marginTop: "300px", color: "white" }} >
-                  <span><h2><i>Test Your Knowledge</i></h2></span>
-                  <span><h2><i>Challange Your Limits</i></h2></span>
-                </div>
-                <div className="container " style={{ marginLeft: "200px", marginTop: "10px", color: "white" }} >
-                  <h3>Answer</h3>
-                  <h3>Learn</h3>
-                  <h3>Repeat</h3>
-                </div>
+              <div className="container " style={{ marginLeft: "200px", marginTop: "300px", color: "white" }} >
+                <span><h2><i>Test Your Knowledge</i></h2></span>
+                <span><h2><i>Challange Your Limits</i></h2></span>
+              </div>
+              <div className="container " style={{ marginLeft: "200px", marginTop: "10px", color: "white" }} >
+                <h3>Answer</h3>
+                <h3>Learn</h3>
+                <h3>Repeat</h3>
               </div>
             </div>
           </div>
-          <div className="container-fluid" style={{ backgroundColor: "bisque", height: "100vh", width: "100vh" }}>
-            <div className="row">
-              <div className="col-lg-12">
-                <p>hhhhi</p>
+          <div className="container-fluid col-lg-6" style={{ backgroundColor: "hsl(194, 30%, 86%)", height: "100vh", width: "100vh" }}>
+            <div className="row col-lg-12" style={{ height: "100vh", width: "100vh" }}>
+              <div className="d-flex  row " >
+                <div className="">
+                  <img src="src/assets/Q.png" alt="" style={{ height: "100px", width: "100px" }} />
+                  <div className="" style={{ marginLeft: "160px", width: "80vh", color: "black" }}><h3><strong>Quiz Fest</strong></h3> </div>
+                </div>
+                <div className="d-flex p-2 align-items-end" style={{ marginLeft: "160px", width: "80vh", color: "black" }}><h4><strong>Login</strong></h4></div>
               </div>
+              <div>
+                <div className="container row col-lg-6" style={{ marginLeft: "130px" }}>
+                  <div class="form-outline w-100">
+                    <label class="form-label" for="input1">Email</label>
+                    <input type="text" id="input1" class="form-control" />
+                  </div>
+                  <div class="form-outline w-100">
+                    <label class="form-label" for="input1">Password</label>
+                    <input type="text" id="input1" class="form-control" />
+                    <button className="btn btn-primary" style={{ marginTop: "10px" }}>Login</button>
+                    <Link className="d-flex" style={{ marginTop: "10px" }}>Register</Link>
+                  </div>
+                </div>
+              </div>
+              <div></div>
             </div>
           </div>
         </div>
